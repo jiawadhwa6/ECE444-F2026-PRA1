026a70a257d0e37abcbe5ab4cc76905361614043
jiawadhwa6 <164456603+jiawadhwa6@users.noreply.github.com>
2026-09-17 01:03:18 -0400

    fixed type error checking in unit test

--- utils.py
+++ utils.py
@@ -1,10 +1,13 @@
 class utils:
 
     def reversed(self, number):
+        if not isinstance(number, int):
+            raise TypeError("number must be an integer")
         return int(str(number)[::-1])
 
     def formatter(self, number):
-        number = int(number)
+        if not isinstance(number, int):
+            raise TypeError("number must be an integer")
         return bin(number), oct(number)
 
 # print(utils().reversed(12345))
--- utils_tests.py
+++ utils_tests.py
@@ -7,23 +7,31 @@ class TestUtils(unittest.TestCase):
     def setUp(self):
         self.util = utils()
 
-    def test_reversed_string(self):
-        self.assertEqual(self.util.reversed("1234"), 4321)
-
-    def test_reversed_float(self):
-        self.assertEqual(self.util.reversed(1234.0), 4321)
+    # ---------- reversed() ----------
 
     def test_reversed_integer(self):
         self.assertEqual(self.util.reversed(1234), 4321)
 
-    def test_formatter_string(self):
-        self.assertEqual(self.util.formatter("10"), ("0b1010", "0o12"))
+    def test_reversed_string_raises_type_error(self):
+        with self.assertRaises(TypeError):
+            self.util.reversed("1234")
 
-    def test_formatter_float(self):
-        self.assertEqual(self.util.formatter(10.0), ("0b1010", "0o12"))
+    def test_reversed_float_raises_type_error(self):
+        with self.assertRaises(TypeError):
+            self.util.reversed(1234.0)
+
+    # ---------- formatter() ----------
 
     def test_formatter_integer(self):
         self.assertEqual(self.util.formatter(10), ("0b1010", "0o12"))
+
+    def test_formatter_string_raises_type_error(self):
+        with self.assertRaises(TypeError):
+            self.util.formatter("10")
+
+    def test_formatter_float_raises_type_error(self):
+        with self.assertRaises(TypeError):
+            self.util.formatter(10.0)
 
 
 if __name__ == "__main__":
